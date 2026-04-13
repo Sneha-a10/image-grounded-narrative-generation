77a0828867169e7a1aa431c8baeb76d302c67329
Pushan T <pushan.sahanaganesh@gmail.com>
2026-04-13 10:38:15 +0530

    resolve merge conflicts

--- tts.py
+++ tts.py
@@ -5,6 +5,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+"""
+sample input text:
+
+"Fujifilm cameras are my favourite DSLRs, their built in film simulations make it so much easier to take pretty photos"
+
+This includes the word "Fujifilm" which is not a usual word in english, this ensures that the model is able to guess the correct way to pronounce words that are unlikely to be in the training set.
+
+"DSLRs" is an abbreveation and is usually pronounced as "dee-es-el-ars" where the final 's' is concatenated with the pronunciation of the letter 'R' while the remaining letters are pronounced individually.
+
+"""
+
+
 input_text = (
 	"Fujifilm cameras are my favourite DSLRs, their built in film simulations "
 	"make it so much easier to take pretty photos"
